2991472ff04b41e0c92774557ab3b32dec2228f2
neorusse <neorusse@gmail.com>
2019-12-17 20:34:20 +0100

    Project setup

---
+++ server/app.ts
@@ -0,0 +1,44 @@
+import createError from "http-errors";
+import express, { Application } from "express";
+import cors from "cors";
+import compression from "compression";
+import logger from "morgan";
+
+// Initialize the Express App
+const app: Application = express();
+
+// logs HTTP requests
+app.use(logger("dev"));
+
+app.disable("x-powered-by");
+app.use(compression());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Root Route
+app.get("/", (_req, res) => {
+  res.status(200).json({ info: "Welcome to UduPay" });
+});
+
+// catch 404 and forward to error handler
+app.use(function(
+  _req: express.Request,
+  _res: express.Response,
+  next: express.NextFunction
+) {
+  next(createError(404));
+});
+
+// error handler
+app.use((err: any, req: express.Request, res: express.Response) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+export default app;
