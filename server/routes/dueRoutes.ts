fc01e91709c6a006bc072d616ddcc2370b5cfb75
neorusse <neorusse@gmail.com>
2019-12-30 16:18:10 +0100

    [starts #170465516] Build route for creating a due

---
+++ server/routes/dueRoutes.ts
@@ -0,0 +1,17 @@
+import { Request, Response, Router } from "express";
+
+import { validateCreateDue, validate } from "../helpers/validator";
+
+import { createADue } from "../controllers/dueController";
+
+import { adminAuth } from "../middleware/auth";
+
+const router = Router();
+
+/** PROTECT ALL ROUTES USING THIS MIDDLEWARE */
+//router.use(adminAuth);
+
+// create a due
+router.post("/create", validateCreateDue(), validate, createADue);
+
+export default router;
