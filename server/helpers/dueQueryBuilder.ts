0ea214942749a2809f1e2f1e7d274735ae4f0d6c
neorusse <neorusse@gmail.com>
2019-12-30 19:20:59 +0100

    chore: Create getDueById & updateDue db query

--- server/helpers/dueQueryBuilder.ts
+++ server/helpers/dueQueryBuilder.ts
@@ -11,9 +11,21 @@ export async function getDueByName(name: string) {
   return await db.query(sql`SELECT * FROM dues WHERE name=${name}`);
 }
 
+// Retrieve a due by name
+export async function getDueById(id: string) {
+  return await db.query(sql`SELECT * FROM dues WHERE id=${id}`);
+}
+
 // Insert due
 export async function insertDue(name: string, amount: number) {
   return await db.query(sql`
     INSERT INTO dues (name, amount) VALUES (${name}, ${amount}) RETURNING id, name, amount;
   `);
+}
+
+// update a due
+export async function updateDue(dueId: string, name: string, amount: string) {
+  return await db.query(
+    sql`UPDATE dues SET name=${name}, amount=${amount} WHERE id=${dueId} RETURNING id, name, amount`
+  );
 }
