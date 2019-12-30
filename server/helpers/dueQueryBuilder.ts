e91b5ea48e61238f2bb9c6fc68aa08d625d41694
neorusse <neorusse@gmail.com>
2019-12-30 18:32:11 +0100

    chore: Create fetchAllDues db query

--- server/helpers/dueQueryBuilder.ts
+++ server/helpers/dueQueryBuilder.ts
@@ -1,7 +1,12 @@
 import db from "../utils/dbConnect";
 import { sql } from "@databases/pg";
 
-// Retrieve user by Id
+// Retrieve all dues
+export async function fetchAllDues() {
+  return await db.query(sql`SELECT * FROM dues`);
+}
+
+// Retrieve a due by name
 export async function getDueByName(name: string) {
   return await db.query(sql`SELECT * FROM dues WHERE name=${name}`);
 }
