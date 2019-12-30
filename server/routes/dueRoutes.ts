67541f02bdc61afdb10968e6701c43c0786ecfe3
neorusse <neorusse@gmail.com>
2019-12-30 18:11:46 +0100

    refact: modify create a dues route

--- server/routes/dueRoutes.ts
+++ server/routes/dueRoutes.ts
@@ -1,10 +1,10 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
 import { validateCreateDue, validate } from "../helpers/validator";
 
 import { createADue } from "../controllers/dueController";
 
-import { adminAuth } from "../middleware/auth";
+//import { adminAuth } from "../middleware/auth";
 
 const router = Router();
 
