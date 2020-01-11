20e8d7882a57edcadad6d30ea2473392212c78aa
neorusse <neorusse@gmail.com>
2020-01-11 22:43:18 +0100

    ft: Create pages for demo purpose

---
+++ client/src/pages/homepage/homepage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomePage() {
+  return <div className="homepage">Directory</div>;
+}
+
+export default HomePage;
---
+++ client/src/pages/signup/signUp.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SignUp() {
+  return <div className="homepage">SignUp</div>;
+}
+
+export default SignUp;
