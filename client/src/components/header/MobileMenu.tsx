2eec22825f0b45eb018bba46003caf0c78852a14
neorusse <neorusse@gmail.com>
2020-01-11 22:42:03 +0100

    ft: Create mobileview nav menu

---
+++ client/src/components/header/MobileMenu.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import { useSpring, animated } from 'react-spring';
+
+const MobileMenu = ({ handleNavbar, navbarState }) => {
+  const { open } = useSpring({ open: navbarState ? 0 : 1 });
+
+  if (navbarState === true) {
+    return (
+      <MobileMenuContainer
+        style={{
+          transform: open
+            .interpolate({
+              range: [0, 0.2, 0.3, 1],
+              output: [0, -20, 0, -200],
+            })
+            .interpolate(openValue => `translate3d(0, ${openValue}px, 0`),
+        }}
+      >
+        <NavLinks>
+          <Link onClick={handleNavbar} to="/communities">
+            Communities
+          </Link>
+          <Link onClick={handleNavbar} to="/support">
+            Support
+          </Link>
+          <Link onClick={handleNavbar} to="/login">
+            Login
+          </Link>
+          <Link onClick={handleNavbar} to="/register">
+            Create Account
+          </Link>
+        </NavLinks>
+      </MobileMenuContainer>
+    );
+  }
+  return null;
+};
+
+export default MobileMenu;
+
+const MobileMenuContainer = styled(animated.div)`
+  position: fixed;
+  top: 8rem;
+  left: 0;
+  right: 0;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & a {
+    font-size: 15px;
+    line-height: 2;
+    color: #000;
+    cursor: pointer;
+
+    &:hover {
+      color: #fdcb6e;
+      border-bottom: 1px solid #fdcb6e;
+    }
+  }
+`;
