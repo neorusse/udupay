4375a0b7f0dd9015ab08e9972e2bc6264f0c2d96
neorusse <neorusse@gmail.com>
2020-01-11 22:44:57 +0100

    ft: Create header with logo & nav menu

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useSpring, config } from 'react-spring';
+
+import { ReactComponent as Logo } from '../../assets/udupay.svg';
+import BurgerMenu from './BurgerMenu';
+import MobileMenu from './MobileMenu';
+
+import {
+  HeaderContainer,
+  FlexContainer,
+  LogoContainer,
+  NavContainer,
+  BurgerWrapper,
+  NavLink,
+  NavLinkSignIn,
+} from './header.styles';
+
+function Header({ navbarState, handleNavbar }) {
+  const barAnimation = useSpring({
+    from: { transform: 'translate3d(0, -10rem, 0)' },
+    transform: 'translate3d(0, 0, 0)',
+  });
+
+  const linkAnimation = useSpring({
+    from: { transform: 'translate3d(0, 30px, 0)', opacity: 0 },
+    to: { transform: 'translate3d(0, 0, 0)', opacity: 1 },
+    delay: 800,
+    config: config.wobbly,
+  });
+
+  return (
+    <>
+      <HeaderContainer style={barAnimation}>
+        <FlexContainer>
+          <LogoContainer to="/">
+            <Logo className="logo" />
+          </LogoContainer>
+          <NavContainer style={linkAnimation}>
+            <NavLink to="/communities">Communities</NavLink>
+            <NavLink to="/support">Support</NavLink>
+            <NavLink to="/login">Login</NavLink>
+            <NavLinkSignIn to="/register">create account</NavLinkSignIn>
+          </NavContainer>
+          <BurgerWrapper>
+            <BurgerMenu navbarState={navbarState} handleNavbar={handleNavbar} />
+          </BurgerWrapper>
+        </FlexContainer>
+      </HeaderContainer>
+      <MobileMenu navbarState={navbarState} handleNavbar={handleNavbar} />
+    </>
+  );
+}
+
+export default Header;
