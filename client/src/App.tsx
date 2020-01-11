49d30ca049b677a05536e3c3c23f4a95baa0e5fe
neorusse <neorusse@gmail.com>
2020-01-11 22:47:00 +0100

    chore: Render header component & its routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
+
+import Header from './components/header/Header';
+import HomePage from './pages/homepage/homepage';
+import SignIn from './pages/signin/signIn';
+import SignUp from './pages/signup/signUp';
 
 const App: React.FC = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
   return (
-    <div className="App">
-      <h1>Welcome to UduPay</h1>
-    </div>
+    <>
+      <Header navbarState={navbarOpen} handleNavbar={handleNavbar} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={SignIn} />
+        <Route exact path="/register" component={SignUp} />
+      </Switch>
+    </>
   );
 };
 
