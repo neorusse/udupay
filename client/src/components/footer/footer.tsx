a54c39dba44daf755b80efe033c50e919c137c9f
neorusse <neorusse@gmail.com>
2020-01-20 14:58:09 +0100

    fix: Delete footer.styles.tsx component

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import FooterInfo from '../footer-info/footer-info';
 import FooterMedia from '../footer-media/footer-media';
 
-import { FooterContainer } from './footer.styles';
-
 function Footer() {
   return (
-    <FooterContainer>
+    <div>
       <FooterInfo />
       <FooterMedia />
-    </FooterContainer>
+    </div>
   );
 }
 
