f6f1e1949079dae3b6adfefb3aedfefc7aef0e19
neorusse <neorusse@gmail.com>
2020-01-20 14:40:24 +0100

    feat: Render footer component

--- client/src/pages/homepage/homepage.tsx
+++ client/src/pages/homepage/homepage.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Hero from '../../components/hero/hero';
 import Testimonial from '../../components/testimonial/testimonial';
+import Footer from '../../components/footer/footer';
 
 function HomePage() {
   return (
     <div className="homepage">
       <Hero />
       <Testimonial />
+      <Footer />
     </div>
   );
 }
