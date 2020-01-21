144502fa0e5483a7204af13296a4c7b22c3974e1
neorusse <neorusse@gmail.com>
2020-01-21 12:11:09 +0100

    test: E2E testing of app homepage

--- cypress/e2e/udupay.spec.ts
+++ cypress/e2e/udupay.spec.ts
@@ -1,24 +1,18 @@
-// enables intelligent code completion for Cypress commands
-// https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
 
-context("Example Cypress TodoMVC test", () => {
+/* globals cy */
+describe('UduPay React Client', () => {
   beforeEach(() => {
-    // usually we recommend setting baseUrl in cypress.json
-    // but for simplicity of this example we just use it here
-    // https://on.cypress.io/visit
-    cy.visit("/");
+    cy.visit('/');
   });
 
-  it("shows learn link", function() {
-    cy.get(".App-link")
-      .should("be.visible")
-      .and("have.text", "Learn React");
+  it('can visit the app', () => {
+    cy.get('.sc-htoDjs')
+      .click()
+      .get('.sc-htoDjs')
+      .contains('create account')
+      .click()
+      .get('.sc-bZQynM')
+      .click();
   });
-
-  // more examples
-  //
-  // https://github.com/cypress-io/cypress-example-todomvc
-  // https://github.com/cypress-io/cypress-example-kitchensink
-  // https://on.cypress.io/writing-your-first-test
 });
