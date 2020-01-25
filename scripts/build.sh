2fdb33045b3480f05cedc158d39ace283a1d98a5
neorusse <neorusse@gmail.com>
2020-01-25 23:06:13 +0100

    fix: Incude tsc cmd

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,9 @@
 yarn --frozen-lockfile
 
-# cd client
-# export SKIP_PREFLIGHT_CHECK=true
+yarn tsc
 
-# yarn --frozen-lockfile
-# yarn build
+cd client
+export SKIP_PREFLIGHT_CHECK=true
+
+yarn --frozen-lockfile
+yarn build
