c0f0c893b529ef6120a01eac836f4f85d943184b
neorusse <neorusse@gmail.com>
2020-01-27 11:47:26 +0100

    [starts #170919811] Fix responsiveness issue

--- client/src/pages/signup/signup.styles.tsx
+++ client/src/pages/signup/signup.styles.tsx
@@ -1,28 +1,92 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const RegisterContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-gap: 1rem;
+  grid-gap: 1px;
+  grid-template-columns: 2fr 1fr;
+  height: 100vh;
+
+  @media (max-width: 1100px) {
+    display: grid;
+    grid-gap: 1px;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+  }
+
+  @media (max-width: 899px) {
+    display: grid;
+    grid-gap: 1px;
+    grid-template-columns: 1fr 1fr;
+    height: 100vh;
+  }
+
+  @media (max-width: 650px) {
+    display: grid;
+    grid-gap: 1px;
+    grid-template-columns: 0 1fr;
+    height: 100vh;
+
+    .col-sp-2 {
+      display: none;
+    }
+  }
+`;
+
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  padding-top: 20px;
 `;
 
 export const FormContainer = styled.div`
-  margin-top: 20px;
+  grid-column: 2;
+  grid-row: 1;
+  background: linear-gradient(
+    to bottom,
+    rgb(52, 50, 72) 0%,
+    rgb(63, 61, 86) 75%,
+    rgb(63, 61, 86) 100%
+  );
+
+  color: #fff;
 `;
 
 export const RegisterTitle = styled.h3`
-  color: #0f1c70;
-  margin: 5px 0;
-  text-align: center;
+  color: #fff;
 `;
 
 export const ExistingUser = styled.div`
   font-weight: 600;
   margin: 3px 0;
   text-align: center;
+  color: #fff;
 
   span {
     cursor: pointer;
     color: #f5a623;
+  }
+`;
+
+export const ImgDiv = styled.div`
+  grid-column: 1;
+  grid-row: 1;
+  text-align: center;
+  background-color: #ffffff;
+  color: #fff;
+
+  .svg img {
+    padding-top: 95px;
+  }
+
+  .svg {
+    max-width: 550px;
+    margin: auto;
+  }
+
+  @media (max-width: 899px) {
+    svg {
+      width: 200px;
+      height: 200px;
+    }
   }
 `;
