684b2fcb428daed3d445f3973ff99575c3d8ce5f
neorusse <neorusse@gmail.com>
2020-01-27 12:21:02 +0100

    feat: Conditionally render header & footer

--- client/src/components/routes/routes.tsx
+++ client/src/components/routes/routes.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { withRouter, Switch, Route } from 'react-router-dom';
 
 import HomePage from '../../pages/home/home';
 import SignIn from '../../pages/signin/signIn';
 import SignUp from '../../pages/signup/signUp';
 import Dashboard from '../../pages/dashboard/dashboard';
+import Header from '../../layout/header/header';
+import Footer from '../../layout/footer/footer';
 
 import PrivateRoute from './PrivateRoute';
 
-const Routes: React.FC = () => {
+const Routes = withRouter(({ location }) => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
   return (
-    <Switch>
-      <Route path="/register" component={SignUp} />
-      <Route exact path="/login" component={SignIn} />
-      <Route exact path="/" component={HomePage} />
-      <PrivateRoute exact path="/dashboard" component={Dashboard} />
-    </Switch>
+    <>
+      {['/login', '/signup', '/dashboard', '/forgot-password'].includes(
+        location.pathname,
+      ) ||
+        (['/'].includes(location.pathname) && (
+          <Header navbarState={navbarOpen} handleNavbar={handleNavbar} />
+        ))}
+      <Switch>
+        <Route path="/register" component={SignUp} />
+        <Route exact path="/login" component={SignIn} />
+        <Route exact path="/" component={HomePage} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      </Switch>
+      {['/login', '/signup', '/dashboard', '/forgot-password'].includes(
+        location.pathname,
+      ) ||
+        (['/'].includes(location.pathname) && <Footer />)}
+    </>
   );
-};
+});
 
 export default Routes;
