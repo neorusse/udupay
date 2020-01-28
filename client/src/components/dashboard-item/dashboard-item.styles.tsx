d528b498dccb986d992e21d171f2070a5fce3eb2
neorusse <neorusse@gmail.com>
2020-01-28 08:44:32 +0100

    style: Add hover rule to section tabs

--- client/src/components/dashboard-item/dashboard-item.styles.tsx
+++ client/src/components/dashboard-item/dashboard-item.styles.tsx
@@ -8,12 +8,18 @@ height: 180px;
 font-size: 1em;
 padding: 20px;
 margin-bottom: 20px;
-font-weight: 400;
+font-weight: 600;
 background: rgba(203, 207, 210, 0.18);
 border-radius: 8px;
 bottom: 0;
 transition: 300ms ease-in;
     transition-property: all;
 transition-property: bottom, box-shadow;
+
+  &:hover {
+    box-shadow: 3px 10px 24px #ccc;
+    bottom: 6px;
+  }
+
   }
 `;
